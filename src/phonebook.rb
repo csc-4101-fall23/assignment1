dbcd68fd0b12178700c94a2488df91f725816340
Haoxiang Lin <124523713+Life1li@users.noreply.github.com>
2023-09-12 02:02:58 -0500

    Update phonebook.rb

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,20 +1,40 @@
 class PhoneBook
-    def initialize
-    end
+  def initialize
+    @phonebook = []
+  end
 
-    def add(name, number, is_listed)
-        raise Exception, "Not implemented"
-    end
+  def add(name, number, is_listed)
+    return false unless valid_number_format?(number)
+    return false if @phonebook.any? { |entry| entry[:name] == name }
 
-    def lookup(name)
-        raise Exception, "Not implemented"
-    end
+    existing_entry = @phonebook.find { |entry| entry[:number] == number && entry[:listed] }
+    return false if existing_entry && is_listed
 
-    def lookupByNum(number)
-        raise Exception, "Not implemented"
-    end
+    @phonebook << { name: name, number: number, listed: is_listed }
+    true
+  end
 
-    def namesByAc(areacode)
-        raise Exception, "Not implemented"
-    end
+  def valid_number_format?(number)
+    /^\d{3}-\d{3}-\d{4}$/ =~ number
+  end
+
+  def lookup(name)
+    existing_entry = @phonebook.find { |entry| entry[:name] == name && entry[:listed] }
+    return nil if existing_entry.nil?
+
+    existing_entry[:number]
+  end
+
+  def lookupByNum(number)
+    existing_entry = @phonebook.find { |entry| entry[:number] == number && entry[:listed] }
+    return nil if existing_entry.nil?
+
+    existing_entry[:name]
+  end
+
+  def namesByAc(areacode)
+    name_arr = []
+    @phonebook.each { |entry| name_arr << entry[:name] if entry[:number][0, 3] == areacode }
+    name_arr
+  end
 end
